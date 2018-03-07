0ee27055ff0e1361e97f2454aa2045a7700a8f19
Pablo Gonzalez <pablo@Pablos-Mac-mini.local>
2018-03-06 22:05:43 -0600

    Product Catalogue

--- public/js/product-catalog/product-catalog.component.js
+++ public/js/product-catalog/product-catalog.component.js
@@ -4,7 +4,12 @@ angular
     .module('productCatalog')
     .component('productCatalog', {
         templateUrl: 'js/product-catalog/product-catalog.template.html',
-        controller: function ProductCatalogController() {
+        controller: ['$http', '$log', function ProductCatalogController($http, $log) {
+            var self = this;
 
-        }
+            $http.get('/api/products').then(result => {
+                $log.debug(result);
+                self.products = result.data;
+            });
+        }]
     });
